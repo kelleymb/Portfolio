41013832388408f21e4a288440fe65c0b9fb8048
Kelleymb <kelleybuccieri@gmail.com>
2020-03-24 14:23:16 -0400

    first draft commt

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".navitem").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 600);
+    
+});
